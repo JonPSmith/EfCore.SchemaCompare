abefc5919d0fcf056d1d6788de651ab8a8cb5dc5
Lars Weber <l.weber@probat.com>
2023-10-13 08:05:25 +0200

    Use reflection to resolve IDatabaseModelFactory from DbContext

--- EfSchemaCompare/Internal/DatabaseModelFinder.cs
+++ EfSchemaCompare/Internal/DatabaseModelFinder.cs
@@ -1,83 +1,37 @@
 ﻿// Copyright (c) 2022 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
 // Licensed under MIT license. See License.txt in the project root for license information.
 
-using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Scaffolding;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace EfSchemaCompare.Internal;
 
 internal static class DatabaseModelFinder
 {
-    private const string SqlServerProviderName = "Microsoft.EntityFrameworkCore.SqlServer";
-    private const string SqliteProviderName = "Microsoft.EntityFrameworkCore.Sqlite";
-    private const string PostgresSqlProviderName = "Npgsql.EntityFrameworkCore.PostgreSQL";
+	public static IDatabaseModelFactory GetDatabaseModelFactory(this DbContext context)
+	{
+		var providerName = context.Database.ProviderName;
+		var providerAssembly = Assembly.Load(providerName!);
+		var factoryType =
+			providerAssembly.ExportedTypes.First(x => x.GetInterface(nameof(IDatabaseModelFactory)) is not null);
 
-    public static IDatabaseModelFactory GetDatabaseModelFactory(this DbContext context)
-    {
-        var providerName = context.Database.ProviderName;
+		var constructor = factoryType.GetConstructors().First(x => x.IsPublic);
+		var constructorParameters = constructor.GetParameters();
 
-        var logger = context.GetService<IDiagnosticsLogger<DbLoggerCategory.Scaffolding>>();
+		if (constructorParameters.Length == 0)
+			return (IDatabaseModelFactory)constructor.Invoke(null);
 
-        var providerAssembly = Assembly.Load(providerName!);
-        var factoryType = providerAssembly.ExportedTypes.First(x => x.BaseType == typeof(DatabaseModelFactory));
-        
-        object factoryObject;
-        switch (providerName)
-        {
-            case SqliteProviderName:
-                var typeMapper = context.GetService<IRelationalTypeMappingSource>();
-                factoryObject = Activator.CreateInstance(factoryType, logger, typeMapper);
-                break;
-            case SqlServerProviderName:
-            case PostgresSqlProviderName:
-                factoryObject = Activator.CreateInstance(factoryType, logger);
-                break;
-            default:
-                // This is not a known provider. Try creating the factory anyhow and throw if it fails
-                factoryObject = TryCreateUnknownFactory(factoryType, logger, providerName);
-                break;
-        }
+		var dbContextInfrastructure = context.GetInfrastructure();
+		var resolvedParameters = new object[constructorParameters.Length];
+		for (var i = 0; i < constructorParameters.Length; i++)
+		{
+			var requestedParameterInfo = constructorParameters[i];
+			resolvedParameters[i] = dbContextInfrastructure.GetService(requestedParameterInfo.ParameterType);
+		}
 
-        if (factoryObject is IDatabaseModelFactory factory)
-            return factory;
-
-        ThrowException(providerName);
-        return (IDatabaseModelFactory) new object();
-    }
-
-    private static object TryCreateUnknownFactory(Type factoryType,
-        IDiagnosticsLogger<DbLoggerCategory.Scaffolding> logger, string providerName)
-    {
-        try
-        {
-            return Activator.CreateInstance(factoryType);
-        }
-        catch
-        {
-            try
-            {
-                return Activator.CreateInstance(factoryType, logger);
-            }
-            catch
-            {
-                ThrowException(providerName);
-            }
-        }
-
-        return null;
-    }
-
-    [DoesNotReturn]
-    private static void ThrowException(string providerName)
-    {
-        throw new InvalidOperationException(
-            $"It could not find the correct EF Core code to support the {providerName} database.");
-    }
+		return (IDatabaseModelFactory)constructor.Invoke(resolvedParameters);
+	}
 }
